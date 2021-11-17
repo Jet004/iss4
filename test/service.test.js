4dbcde37c8049b3dd4b7b14987759d0820811c90
Jacob Turner <jeturner44@outlook.com>
2021-11-17 21:55:40 +1000

    added tests for domain model relationships and cleanup functions to reset the database for the next test

--- test/service.test.js
+++ test/service.test.js
@@ -1,6 +1,4 @@
-const exp = require('constants')
 const fetch = require('node-fetch');
-const { response } = require('express');
 
 const basePath = 'https://my-bookshop-srv-balanced-lizard-pw.cfapps.us10.hana.ondemand.com'
 
@@ -38,7 +36,30 @@ beforeAll(() => {
         body: JSON.stringify(bookData)
     })
         .then(response => {
-            if(!response.ok) return Promise.reject("setup failed")
+            if(!response.ok) return Promise.reject("setup failed - status code: " + response.status)
+        })
+})
+
+afterAll(() => {
+    // Clean up database - delete all orders created during tests
+    let orderIDs = []
+    return fetch(`${basePath}/catalog/Orders?$select=ID`)
+        .then(response => {
+            if(!response.ok) return Promise.reject("Failed to get orders in cleanup")
+            return response.json()
+        })
+        .then(data => {
+            for(let obj of data.value){
+                orderIDs.push(obj.ID)
+            }
+            orderIDs.forEach(ID => {
+                return fetch(`${basePath}/catalog/Orders(${ID})`, {
+                    method: "DELETE"
+                })
+                    .then(response => {
+                        if(!response.status === 204) return Promise.reject(`Failed to delete order ${ID} during cleanup`)
+                    })
+            })
         })
 })
 
@@ -699,7 +720,7 @@ describe('Route /Orders', () => {
                     return response.json()
                 })
                 .then(data => {
-                    expect(data.value.length).toBeGreaterThan(2)
+                    expect(data.value.length).toBeGreaterThan(1)
                     let testOrder = data.value.find(order => order.ID === "c13d3eec-942e-470d-97b3-e03322136636")
                     expect(testOrder).toBeDefined()
                     expect(testOrder.book_ID).toBe(201)
@@ -901,4 +922,241 @@ describe('Route /Orders', () => {
         })
     })
 
+})
+
+describe('Test Domain Model Relationships', () => {
+    beforeAll(() => {
+        // Create Order with UUID to test domain model relationships
+        return fetch(`${basePath}/catalog/Orders(c13d3eec-942e-470d-97b3-e03322136636)`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "ID": "c13d3eec-942e-470d-97b3-e03322136636",
+                "book_ID": 201,
+                "amount": 10
+              })
+        })
+            .then(response => {
+                if(!response.ok) return Promise.reject("Failed to set up order with UUID for testing domain model relationships")
+                return response.json()
+            })
+    })
+
+    it('GET /Orders?$expand=book - responds with all orders with book details', () => {
+        return fetch(`${basePath}/catalog/Orders?$expand=book`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                let targetOrder = data.value.find(order => order.ID === "c13d3eec-942e-470d-97b3-e03322136636")
+                expect(targetOrder.ID).toBe("c13d3eec-942e-470d-97b3-e03322136636")
+                expect(targetOrder.book_ID).toBe(201)
+                expect(targetOrder.book.author_ID).toBe(101)
+            })
+    })
+
+    it('GET /Orders?$expand=book($expand=author) - responds with all orders with book and author details', () => {
+        return fetch(`${basePath}/catalog/Orders?$expand=book($expand=author)`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                let targetOrder = data.value.find(order => order.ID === "c13d3eec-942e-470d-97b3-e03322136636")
+                expect(targetOrder.ID).toBe("c13d3eec-942e-470d-97b3-e03322136636")
+                expect(targetOrder.book_ID).toBe(201)
+                expect(targetOrder.book.author.ID).toBe(101)
+            })
+    })
+
+    it('GET /Books(201)/author - with valid book ID - responds with 200 OK', () => {
+        return fetch(`${basePath}/catalog/Books(201)/author`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.ID).toBe(101)
+                expect(data.name).toBe("Emily Brontë")
+            })
+    })
+
+    it('GET /Books(someString)/author - with invalid book ID - responds with 404 Not Found', () => {
+        return fetch(`${basePath}/catalog/Books(someString)/author`)
+            .then(response => {
+                expect(response.status).toBe(404)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("404")
+                expect(data.error.message).toMatch(/is not a valid key/)
+            })
+    })
+
+    it('GET /Books()/author - with missing book ID - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Books()/author`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+                expect(data.error.message).toMatch(/Expected at least one key/)
+            })
+    })
+
+    it('GET /Books/author - with missing parameter - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Books/author`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+            })
+    })
+
+    it('GET /Authors(101)/books - with valid author ID - responds with 200 OK', () => {
+        return fetch(`${basePath}/catalog/Authors(101)/books`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                let target = data.value.find(x => x.ID === 201)
+                expect(target.ID).toBe(201)
+                expect(target.title).toBe("Wuthering Heights")
+            })
+    })
+
+    it('GET /Authors(someString)/books - with invalid author ID - responds with 404 Not Found', () => {
+        return fetch(`${basePath}/catalog/Authors(someString)/books`)
+            .then(response => {
+                expect(response.status).toBe(404)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("404")
+                expect(data.error.message).toMatch(/is not a valid key/)
+            })
+    })
+
+    it('GET /Authors()/books - with missing author ID - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors()/books`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+                expect(data.error.message).toMatch(/Expected at least one key/)
+            })
+    })
+
+    it('GET /Authors/books - with missing parameter - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors/books`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+            })
+    })
+
+    it('GET /Authors(101)/name - with valid author ID - responds with 200 OK', () => {
+        return fetch(`${basePath}/catalog/Authors(101)/name`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.value).toBe("Emily Brontë")
+            })
+    })
+
+    it('GET /Authors(someString)/name - with invalid author ID - responds with 404 Not Found', () => {
+        return fetch(`${basePath}/catalog/Authors(someString)/name`)
+            .then(response => {
+                expect(response.status).toBe(404)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("404")
+                expect(data.error.message).toMatch(/is not a valid key/)
+            })
+    })
+
+    it('GET /Authors()/name - with missing author ID - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors()/name`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+                expect(data.error.message).toMatch(/Expected at least one key/)
+            })
+    })
+
+    it('GET /Authors/name - with missing parameter - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors/name`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+            })
+    })
+
+    it('GET /Authors(101)/books?$select=title - with valid author ID - responds with 200 OK', () => {
+        return fetch(`${basePath}/catalog/Authors(101)/books?$select=title`)
+            .then(response => {
+                expect(response.status).toBe(200)
+                return response.json()
+            })
+            .then(data => {
+                let book = data.value.find(x => x.ID === 201)
+                expect(book.title).toBe("Wuthering Heights")
+            })
+    })
+
+    it('GET /Authors(someString)/books?$select=title - with invalid author ID - responds with 404 Not Found', () => {
+        return fetch(`${basePath}/catalog/Authors(someString)/books?$select=title`)
+            .then(response => {
+                expect(response.status).toBe(404)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("404")
+                expect(data.error.message).toMatch(/is not a valid key/)
+            })
+    })
+
+    it('GET /Authors()/books?$select=title - with missing author ID - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors()/books?$select=title`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+                expect(data.error.message).toMatch(/Expected at least one key/)
+            })
+    })
+
+    it('GET /Authors/books?$select=title - with missing parameter - responds with 400 Bad Request', () => {
+        return fetch(`${basePath}/catalog/Authors/books?$select=title`)
+            .then(response => {
+                expect(response.status).toBe(400)
+                return response.json()
+            })
+            .then(data => {
+                expect(data.error.code).toBe("400")
+            })
+    })
 })
